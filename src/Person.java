3dfac277bad9cdce0e91aef9c458cfd21b7ed5c8
Olena <olena.pyvovar1234@gmail.com>
2019-02-23 13:29:57 +0200

    Add age

--- src/Person.java
+++ src/Person.java
@@ -3,21 +3,24 @@ import java.util.Scanner;
 public class Person {
 
 	public static void main(String[] args) {
-		System.out.println("Enter your name: ");
+		System.out.println("Enter your name and age: ");
 		Scanner sc = new Scanner(System.in);
 		String name = sc.nextLine();
-		Person person = new Person(name);
-		person.greeting();
+		int age = sc.nextInt();
+		Person person = new Person(name, age);
 	}
 	
 	private String name;
+	private int age;
 	
-	public Person(String name) {
+	public Person(String name, int age) {
 		this.name = name;
+		this.age = age;
+		greeting();
 	}
 	
 	private void greeting() {
-		System.out.println("Hello, " + name);
+		System.out.println("Hello, " + name + ", your age: " + age);
 	}
 
 }
