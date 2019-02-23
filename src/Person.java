20ee2e0efa20ac5d3ad49ab0d94a3355b9f83e82
Tetiana <tanya.rovnina@gmail.com>
2019-02-23 13:21:35 +0200

    Add age. Change gritings in program

--- src/Person.java
+++ src/Person.java
@@ -6,18 +6,22 @@ public class Person {
 		System.out.println("Enter your name: ");
 		Scanner sc = new Scanner(System.in);
 		String name = sc.nextLine();
-		Person person = new Person(name);
+		System.out.println("Enter your age: ");
+		int age = sc.nextInt();
+		Person person = new Person(name, age);
 		person.greeting();
 	}
 	
 	private String name;
+	private String age;
 	
-	public Person(String name) {
+	public Person(String name, int age) {
 		this.name = name;
+		this.age = age;
 	}
 	
 	private void greeting() {
-		System.out.println("Hello, " + name);
+		System.out.println("Hello, " + name \nFor your " + age + " years to write 'Hello, someone' is not the best desision : )");
 	}
 
 }
